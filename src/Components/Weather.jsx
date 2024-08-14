596fb214f59c159df92928b154106280c638b008
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2024-08-14 10:06:29 +0700

    Styling temperature, location and weather-data class

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png';
+
+// icons for weather
+import clear_icon from '../assets/clear.png';
+import cloud_icon from '../assets/cloud.png';
+import drizzle_icon from '../assets/drizzle.png';
+import rain_icon from '../assets/rain.png';
+import snow_icon from '../assets/search.png';
+import wind_icon from '../assets/wind.png';
+import humidity_icon from '../assets/humidity.png';
 export default function Weather() {
   return (
     <div className='weather'>
         <div className="search-bar">
             <input type="text" placeholder='Search' />
             <img src={search_icon} alt="" />
+        </div>
+        <img src={clear_icon} alt="" className='weather-icon' />
+        <p className='temperature'>16°c</p>
+        <p className='location'>London</p>
+        <div className="weather-data">
+            <div className="col">
+                <img src={humidity_icon} alt="" />
+                <div>
+                    <p>91 %</p>
+                    <span>Humadity</span>
+                </div>
+            </div>
+            <div className="col">
+                <img src={wind_icon} alt="" />
+                <div>
+                    <p>3.6 Km/h</p>
+                    <span>Wind Speed</span>
+                </div>
+            </div>
         </div>
     </div>
   )
