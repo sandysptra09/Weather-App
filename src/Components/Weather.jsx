25cedd7192f55261f89b5dfe126e7bfa6268579f
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2024-08-14 13:09:43 +0700

    Update

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -8,7 +8,7 @@ import clear_icon from '../assets/clear.png';
 import cloud_icon from '../assets/cloud.png';
 import drizzle_icon from '../assets/drizzle.png';
 import rain_icon from '../assets/rain.png';
-import snow_icon from '../assets/search.png';
+import snow_icon from '../assets/snow.png';
 import wind_icon from '../assets/wind.png';
 import humidity_icon from '../assets/humidity.png';
 export default function Weather() {
